1dec63267abdbe99d3b2a6706cab948e1c6e3ff6
bnaren <bnaren@yahoo.com>
2020-05-03 20:08:21 -0700

    Added extended GCD function

--- Math.py
+++ Math.py
@@ -1,5 +1,5 @@
 
-# Euclid Algorithm
+# Find the GCD based on the Euclid Algorithm
 def gcd_euclid(a, b):
     assert a >= 0 and b >= 0
     while a > 0 and b > 0:
@@ -8,3 +8,22 @@ def gcd_euclid(a, b):
         else:
             b = b%a
     return max(a,b)
+
+def extended_gcd(a, b):
+    # ax + by = d
+    # given: a, b
+    # find x, y and d
+    assert a >= 0 and b >= 0
+
+    if b == 0:
+        d, x, y = a, 1, 0
+    else:
+        (d, p, q) = extended_gcd(b, a % b)
+        x = q
+        y = p - q * (a // b)
+
+    assert a % d == 0 and b % d == 0
+    assert d == a * x + b * y
+
+    return (d, x, y)
+
